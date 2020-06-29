706a3327adcdfc517675bd61e9d001810fea24a5
Myka Kugaya <mykakug129@gmail.com>
2020-06-29 11:04:31 -0700

    app.js router created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Forum from "./pages/Forum";
+import Calendar from "./pages/Calendar";
+import Gallery from "./pages/Gallery";
+import Login from "./pages/Login";
+import Jobs from "./pages/Jobs";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/forum">
+            <Forum />
+          </Route>
+          <Route exact path="/calendar">
+            <Calendar />
+          </Route>
+          <Route exact path="/gallery">
+            <Gallery />
+          </Route>
+          <Route exact path="/jobs">
+            <Jobs />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
