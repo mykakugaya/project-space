2dd3122722137c585bdf542563625a3a8bc65a17
Arash B <abazrafshan123@gmail.com>
2020-06-30 14:15:34 -0500

    Fixed Mars Rover photo gallery functionality

--- client/src/utils/AsteroidAPI.js
+++ client/src/utils/AsteroidAPI.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import moment from "moment"
 const baseURL = "https://api.nasa.gov/";
-const apikey = "api_key=OsxKBADdC2NEduQmMsJU9kxQTfLlc2vQYdtC4XFE";
-const asteroidApiQuery = "neo/rest/v1/feed?start_date=2020-06-29&"
+const apikey = "&api_key=OsxKBADdC2NEduQmMsJU9kxQTfLlc2vQYdtC4XFE";
+const asteroidApiQuery = "neo/rest/v1/feed?start_date="
+const currentday = moment().format("YYYY-MM-DD");
 
 export default {
     ASTEROIDapisearch: function(){
-        return axios.get(baseURL + asteroidApiQuery + apikey);
+        return axios.get(baseURL + asteroidApiQuery + currentday + apikey);
     }
 };
 // How can I get this api call to query results from today?
