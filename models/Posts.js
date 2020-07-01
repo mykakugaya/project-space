4cc788d0c3105d2d9b7c4ff9c70978bb6091a153
Sharlene May <sharleneminosa@gmail.com>
2020-07-01 12:42:07 -0700

    added Posts model

---
+++ models/Posts.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+    const Post = sequelize.define("Post", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    },
+        {
+ 
+        freezeTableName: true
+    });
+
+
+
+    Post.associate = function(models) {
+        Post.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Post;
+}
