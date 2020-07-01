0bb3cd7de1363ccf2ed7353191e7c613f9c4632f
Sharlene May <sharleneminosa@gmail.com>
2020-07-01 12:02:08 -0700

    API.js user axios

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,6 +7,11 @@ export const searchImage = q => axios.get(`/api/nasa/image?q=${q}`);
 export const getAPOD = () => axios.get("/api/nasa/apod");
 export const getJobs = () => axios.get("/api/nasa/jobs");
 export const getRover = () => axios.get("/api/nasa/rover");
+export const postLogin = () => axios.post("/api/login");
+export const getUsers = () => axios.get("/api/users");
+export const postSignup = () => axios.post("/api/signup");
+export const getLogout = () => axios.get("/api/logout")
+export const getUserData = () => axios.get("/api/user_data");
 
 //import {getAPOD, getJob} from "/path to API.js"
 //getAPOD().then(data=> do something with data;)
