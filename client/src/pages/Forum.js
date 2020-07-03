5858ac189399370088a8f0e883e4e0ad1be6edf1
Myka Kugaya <mykakug129@gmail.com>
2020-07-03 15:19:49 -0700

    forum post component and post response started

--- client/src/pages/Forum.js
+++ client/src/pages/Forum.js
@@ -7,8 +7,20 @@ import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { red } from '@material-ui/core/colors';
 import { createNewPost, getAllPosts } from "../utils/API";
 import {userContext} from "../utils/userContext";
+import moment from "moment";
+const currentday = moment().format("YYYY-MM-DD");
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -31,6 +43,9 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
       padding: "5px"
+  },
+  avatar: {
+    backgroundColor: red[500],
   }
 }));
 
@@ -59,7 +74,8 @@ function Forum() {
       createNewPost( {
           title: newPostTitle,
           category: "default",
-          body: newPostBody
+          body: newPostBody,
+          UserId: user.id
       })
       .then ( () => {
         console.log("New Post Saved");
@@ -70,6 +86,7 @@ function Forum() {
   const getPosts = () => {
     getAllPosts()
     .then(res => {
+      console.log(res)
       setPosts(res.data);
     })
     .catch ( err => setError(err));
@@ -104,39 +121,56 @@ function Forum() {
         <Container>
           <Paper className={classes.paper}>
             <Grid className={classes.form} item xs={12}>
-              <form>
-                <TextField className={classes.text}
-                    id="outlined-multiline-static"
-                    label="Title"
-                    multiline
-                    rows={1}
-                    variant="outlined"
-                    value={newPostTitle}
-                    onChange={e => setNewPostTitle(e.target.value)}
+            <Card>
+                <CardHeader
+                    avatar={
+                    <Avatar aria-label="user" className={classes.avatar}>
+                        S
+                    </Avatar>
+                    }
+                    title={user ? user.name : "Please log in to post."}
+                    subheader={currentday}
                 />
-                <br />
-                <TextField className={classes.text}
-                    id="outlined-multiline-static"
-                    label="Post Content"
-                    multiline
-                    rows={5}
-                    variant="outlined"
-                    value={newPostBody}
-                    onChange={e => setnewPostBody(e.target.value)}
-                />
-                <br />
-                <Button className={classes.button}
-                  variant="contained" 
-                  color="primary" 
-                  disableElevation disabled={!validatePost()}
-                  onClick={handleCreatePost}
-                >Post
-                </Button>
-              </form>
+                <Divider/>
+                <CardContent>
+                  <Typography variant="h6" component="h3">
+                      New Post
+                  </Typography>
+                  <form>
+                    <TextField className={classes.text}
+                        id="outlined-multiline-static"
+                        label="Title"
+                        multiline
+                        rows={1}
+                        variant="outlined"
+                        value={newPostTitle}
+                        onChange={e => setNewPostTitle(e.target.value)}
+                    />
+                    <br />
+                    <TextField className={classes.text}
+                        id="outlined-multiline-static"
+                        label="Post Content"
+                        multiline
+                        rows={5}
+                        variant="outlined"
+                        value={newPostBody}
+                        onChange={e => setnewPostBody(e.target.value)}
+                    />
+                    <br />
+                    <Button className={classes.button}
+                      variant="contained" 
+                      color="primary" 
+                      disableElevation disabled={!validatePost()}
+                      onClick={handleCreatePost}
+                    >Post
+                    </Button>
+                  </form>
+                </CardContent>
+            </Card>
             </Grid>
           </Paper>
           {posts.map(post => {
-            return <Post key={post.id} title={post.title} body={post.body} author={post.author}/>
+            return <Post key={post.id} date = {post.createdAt} title={post.title} category={post.category} body={post.body} author={post.author}/>
           })}
         </Container>
       </Grid>
