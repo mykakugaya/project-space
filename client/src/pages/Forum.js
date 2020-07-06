58733ef9a2775e278b3ac524fcd7cb159208076d
Sharlene May <sharleneminosa@gmail.com>
2020-07-06 08:44:54 -0700

    forum.js added category menu uncomplete

--- client/src/pages/Forum.js
+++ client/src/pages/Forum.js
@@ -7,23 +7,23 @@ import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { red } from '@material-ui/core/colors';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { red } from "@material-ui/core/colors";
 import { createNewPost, getAllPosts } from "../utils/API";
-import {userContext} from "../utils/userContext";
+import { userContext } from "../utils/userContext";
 import moment from "moment";
 const currentday = moment().format("YYYY-MM-DD");
 
@@ -41,13 +41,13 @@ const useStyles = makeStyles((theme) => ({
   },
   text: {
     width: "100ch",
-    padding: "5px"
+    padding: "5px",
   },
   form: {
-      alignContent: "center"
+    alignContent: "center",
   },
   button: {
-      padding: "5px"
+    padding: "5px",
   },
   formControl: {
     margin: theme.spacing(1),
@@ -55,9 +55,8 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     backgroundColor: red[500],
-  }
+  },
 }));
-
 
 function Forum() {
   const classes = useStyles();
@@ -66,41 +65,42 @@ function Forum() {
   const [newPostTitle, setNewPostTitle] = useState("");
   const [newPostBody, setnewPostBody] = useState("");
   const [newPostCategory, setnewPostCategory] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState( "" );
   const [error, setError] = useState("");
 
-  const {user} = useContext(userContext);
+  const { user } = useContext(userContext);
 
   useEffect(() => {
     getPosts(posts);
   }, [posts]);
 
   const validatePost = () => {
-      return newPostTitle.length > 0 && newPostBody.length > 0;
-  }
+    return newPostTitle.length > 0 && newPostBody.length > 0;
+  };
 
-  const handleCreatePost = event => {
-      event.preventDefault();
-      if(!user) {
-        console.log("You must log in or create an account.")
-      }
-      createNewPost( {
-          title: newPostTitle,
-          category: newPostCategory,
-          body: newPostBody,
-          UserId: user.id
-      })
-      .then ( () => {
+  const handleCreatePost = (event) => {
+    event.preventDefault();
+    if (!user) {
+      console.log("You must log in or create an account.");
+    }
+    createNewPost({
+      title: newPostTitle,
+      category: newPostCategory,
+      body: newPostBody,
+      UserId: user.id,
+    })
+      .then(() => {
         console.log("New Post Saved");
       })
-      .catch ( err => setError(err));
-  }
+      .catch((err) => setError(err));
+  };
 
   const getPosts = () => {
     getAllPosts()
-    .then(res => {
-      setPosts(res.data);
-    })
-    .catch ( err => setError(err));
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => setError(err));
   };
 
   const handleInputChange = (event) => {
@@ -132,79 +132,135 @@ function Forum() {
         <Container>
           <Paper className={classes.paper}>
             <Grid className={classes.form} item xs={12}>
-            <Card>
+              <Card>
                 <CardHeader
-                    // avatar={ user ?
-                    // <Avatar aria-label="user" className={classes.avatar}>
-                    //   {user.name[0]}
-                    // </Avatar>
-                    //   :
-                    avatar={
+                  // avatar={ user ?
+                  // <Avatar aria-label="user" className={classes.avatar}>
+                  //   {user.name[0]}
+                  // </Avatar>
+                  //   :
+                  avatar={
                     <Avatar aria-label="user" className={classes.avatar}>
-                        S
+                      S
                     </Avatar>
-                    }
-                    title={user ? user.name : "Please log in to post."}
-                    subheader={currentday}
+                  }
+                  title={user ? user.name : "Please log in to post."}
+                  subheader={currentday}
                 />
-                <Divider/>
+                <Divider />
                 <CardContent>
                   <Typography variant="h6" component="h3">
-                      New Post
+                    New Post
                   </Typography>
                   <form>
-                    <FormControl variant="outlined" className={classes.formControl}>
-                      <InputLabel id="demo-simple-select-outlined-label">Category</InputLabel>
+                    <FormControl
+                      variant="outlined"
+                      className={classes.formControl}
+                    >
+                      <InputLabel id="demo-simple-select-outlined-label">
+                        Category
+                      </InputLabel>
                       <Select
                         labelId="demo-simple-select-outlined-label"
                         id="demo-simple-select-outlined"
                         value={newPostCategory}
-                        onChange={e => setnewPostCategory(e.target.value)}
+                        onChange={(e) => setnewPostCategory(e.target.value)}
                         label="Category"
                       >
                         <MenuItem value={"Earth"}>Earth</MenuItem>
-                        <MenuItem value={"Solar System"}>Solar System</MenuItem>
+                        <MenuItem value={"Solar System"}>
+                          Solar System</MenuItem>
                         <MenuItem value={"NASA"}>NASA</MenuItem>
-                        <MenuItem value={"SpaceX Launches"}>SpaceX Launches</MenuItem>
+                        <MenuItem value={"SpaceX Launches"}>
+                          SpaceX Launches
+                        </MenuItem>
                       </Select>
                     </FormControl>
-                    <TextField className={classes.text}
-                        id="outlined-multiline-static"
-                        label="Title"
-                        multiline
-                        rows={1}
-                        variant="outlined"
-                        value={newPostTitle}
-                        onChange={e => setNewPostTitle(e.target.value)}
+                    <TextField
+                      className={classes.text}
+                      id="outlined-multiline-static"
+                      label="Title"
+                      multiline
+                      rows={1}
+                      variant="outlined"
+                      value={newPostTitle}
+                      onChange={(e) => setNewPostTitle(e.target.value)}
                     />
                     <br />
-                    <TextField className={classes.text}
-                        id="outlined-multiline-static"
-                        label="Post Content"
-                        multiline
-                        rows={5}
-                        variant="outlined"
-                        value={newPostBody}
-                        onChange={e => setnewPostBody(e.target.value)}
+                    <TextField
+                      className={classes.text}
+                      id="outlined-multiline-static"
+                      label="Post Content"
+                      multiline
+                      rows={5}
+                      variant="outlined"
+                      value={newPostBody}
+                      onChange={(e) => setnewPostBody(e.target.value)}
                     />
                     <br />
-                    <Button className={classes.button}
-                      variant="contained" 
-                      color="primary" 
-                      disableElevation disabled={!validatePost()}
+                    <Button
+                      className={classes.button}
+                      variant="contained"
+                      color="primary"
+                      disableElevation
+                      disabled={!validatePost()}
                       onClick={handleCreatePost}
-                    >Post
+                    >
+                      Post
                     </Button>
                   </form>
                 </CardContent>
-            </Card>
+              </Card>
             </Grid>
           </Paper>
-          {posts.map(post => {
-            const date = post.createdAt.slice(0, 10) + " at " + post.createdAt.slice(11,16)
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={filteredPosts}
+            onChange={(e) => setFilteredPosts(e.target.value)}
+            label="Search Posts by Category"
+          >
+            <MenuItem value={"Earth"}>Earth</MenuItem>
+            <MenuItem value={"Solar System"}>Solar System</MenuItem>
+            <MenuItem value={"NASA"}>NASA</MenuItem>
+            <MenuItem value={"SpaceX Launches"}>SpaceX Launches</MenuItem>
+          </Select>
+          {
+        //   filteredPosts ? 
+        //     posts.filter(post => {
+        //       post.category == filteredPosts;
+        //       const date =
+        //         post.createdAt.slice(0, 10) +
+        //         " at " +
+        //         post.createdAt.slice(11, 16);
+        //     return (
+        //       <Post
+        //         key={post.id}
+        //         id={post.id}
+        //         date={date}
+        //         title={post.title}
+        //         category={post.category}
+        //         body={post.body}
+        //         author={post.User.name}
+        //       />
+        //     );
+        //   }) : 
+          posts.map((post) => {
+            const date =
+              post.createdAt.slice(0, 10) +
+              " at " +
+              post.createdAt.slice(11, 16);
             return (
-              <Post key={post.id} id={post.id} date ={date} title={post.title} category={post.category} body={post.body} author={post.User.name}/>
-            )
+              <Post
+                key={post.id}
+                id={post.id}
+                date={date}
+                title={post.title}
+                category={post.category}
+                body={post.body}
+                author={post.User.name}
+              />
+            );
           })}
         </Container>
       </Grid>
