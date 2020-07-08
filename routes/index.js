132a2fa6c5d7bf97ea53d98d226552dbb6d2455f
Myka Kugaya <mykakug129@gmail.com>
2020-07-08 09:29:51 -0700

    server fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const imageRoutes = require("./ImageAPI");
 const postRoutes = require("./PostsAPI");
 const responseRoutes = require("./ResponseAPI");
 const jobRoutes = require("./JobsAPI");
-const path = require("path");
 
 router.use("/nasa", NASARoutes);
 router.use(AuthRoutes);
@@ -13,8 +12,5 @@ router.use(imageRoutes);
 router.use(postRoutes);
 router.use(responseRoutes);
 router.use(jobRoutes);
-router.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
-})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 // set up ports and requiring models for syncing
 const PORT = process.env.PORT || 3001;
 const db = require("./models");
+const path = require('path')
 
 //im a placeholder
 // Define middleware here
@@ -35,6 +36,10 @@ function shouldCompress (req, res) {
 }
 // Add routes, both API and view
 app.use("/api", routes);
+
+app.get("*", (req,res)=>{
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 
 db.sequelize.sync({force:false}).then(() => { 
   app.listen(PORT, () => {
