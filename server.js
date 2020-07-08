b5836e9ac17e458d24c287d384769bd78e2f50c9
Myka Kugaya <mykakug129@gmail.com>
2020-07-08 09:39:55 -0700

    server

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function shouldCompress (req, res) {
 app.use("/api", routes);
 
 app.get("*", (req,res)=>{
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
 db.sequelize.sync({force:false}).then(() => { 
